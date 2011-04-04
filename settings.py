469226dbc7fc2f6337243137b5f0287c66666bc0
Nick Sullivan <nick@kruxdigital.com>
2011-04-03 22:05:33 -0700

    template pulled over

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for fd project.
+import os
 
+ROOT_PATH = os.path.dirname(__file__)
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -27,7 +29,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -37,11 +39,11 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
-USE_L10N = True
+USE_L10N = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
@@ -56,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = ROOT_PATH + '/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -103,9 +105,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'fd.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT_PATH, "templates"),
 )
 
 INSTALLED_APPS = (
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
@@ -6,7 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fd.views.home', name='home'),
     # url(r'^fd/', include('fd.foo.urls')),
-
+    
     (r'^profiles/', include('userena.urls')),
     url(r'^admin/', include(admin.site.urls))
 )
+urlpatterns += staticfiles_urlpatterns()
