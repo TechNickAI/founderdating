26c315bf5dfd0cf2add9d050c91918d0c73dcec9
Nick Sullivan <nick@kruxdigital.com>
2011-04-03 21:05:58 -0700

    initial project + userena

---
+++ profiles/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+from userena.models import UserenaBaseProfile
+
+class FdProfile(UserenaBaseProfile):
+    pass
---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'fd.views.home', name='home'),
+    # url(r'^fd/', include('fd.foo.urls')),
+
+    (r'^profiles/', include('userena.urls')),
+    url(r'^admin/', include(admin.site.urls))
+)
