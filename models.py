e5bcab9f30996f43b46257ca6bc5f30064d0a750
Nick Sullivan <nick@liftium.com>
2011-04-15 02:44:16 -0400

    proper signed in as in template so linkedin login works

--- models.py
+++
@@ -1,5 +0,0 @@
-from userena.models import UserenaBaseProfile
-
-class MyProfile(UserenaBaseProfile):
-    favourite_snack = models.CharField(_('favourite snack'),
-                                       max_length=5)
