500e63f85abd3a4f84b2ccf31162f3890de7bafb
Nick Sullivan <nick@kruxdigital.com>
2011-04-19 19:28:24 -0700

    dynamically display the available locations

--- profiles/context_processors.py
+++ profiles/context_processors.py
@@ -1,5 +1,10 @@
+from fd.profiles.models import EventLocation
+
 def fd_context(request):
-	context = {}
-	context["session"] = request.session
-	context["get"] = request.GET.copy()
-	return context
+    context = {}
+    context["session"] = request.session
+    context["get"] = request.GET.copy()
+
+    # Bring in the event locations for the menu
+    context["event_locations"] = EventLocation.objects.all()
+    return context
