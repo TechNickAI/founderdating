85cfeddb7b012b5cc23bd01fd1831c525bb52f3d
Nick Sullivan <nick@liftium.com>
2011-04-24 19:34:39 -0400

    model work for profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,40 @@
 from django.db import models
-
 from userena.models import UserenaBaseProfile
 
 class FdProfile(UserenaBaseProfile):
-    event = models.ForeignKey('Event', null=True)
+    START_CHOICES = (
+        ('immediately', 'Immediately'),
+        ('part now full soon', 'Part-time now, full-time soon'),
+        ('part now full if no suck', 'Part-time now, full-time if it takes off'),
+        ('later', 'I don\'t have much for the next several months')
+    )
+    IDEA_STATUS_CHOICES = (
+        ('straight', 'I have an idea that I\'m committed to'),
+        ('curious', 'I have and idea, but I\'m also open to exploring other ideas'),
+        ('ambiguous', 'I don\'t yet have an idea')
+    )
+    EVENT_STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('accepted and attending', 'Accepted and Attending'),
+        ('denied', 'Denied')
+    )
+
+    bring_skillsets_json = models.CharField(max_length=255, blank=True, null=True)
+    need_skillsets_json = models.CharField(max_length=255, blank=True, null=True)
+    past_experience_blurb = models.TextField(blank=True, null=True)
+    bring_blurb = models.TextField(blank=True, null=True)
+    can_start = models.CharField(max_length=25, choices = START_CHOICES, blank=True, null=True)
+    idea_status = models.CharField(max_length=25, choices = IDEA_STATUS_CHOICES, blank=True, null=True)
+    event_status = models.CharField(max_length=25, choices =  EVENT_STATUS_CHOICES, default = 'Pending')
+    event = models.ForeignKey('Event', null=True, blank=True)
+
+class LinkedinProfile(models.Model):
+    fd_profile = models.OneToOneField("FdProfile")
+    oauth_object = models.TextField(max_length=500)
+    profile_raw = models.TextField()
+    profile_picture = models.URLField(null=True, blank=True)
+    profile_location = models.CharField(max_length=100)
+    profile_industry = models.CharField(max_length=100)
 
 class Event(models.Model):
     event_date = models.DateField()
