fc9c35b1861e0e83a086194dfe7e505d30df7757
Nick Sullivan <nick@sullivanflock.com>
2011-05-16 21:59:36 -0700

    update admin with linkedin profile

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from profiles.models import Applicant, Event, EventLocation, Interest, Skillset
 
 class ApplicantAdmin(admin.ModelAdmin):
-    list_filter = ['event']
+    def linkedin_link(self, obj):
+        return '<a href="%s" target="_new"><nobr><img src="/static/img/linkedin_icon.png">Profile</nobr></a>' % (obj.linkedin_url)
+
+    linkedin_link.allow_tags = True
+    linkedin_link.short_description = 'LinkedIn'
+
+    list_display = ('name', 'email', 'can_start', 'idea_status', 'event_status', 'linkedin_link', 'event')
+    list_filter = ['event', 'event_status', 'can_start', 'idea_status']
+    date_hierarchy = "created_at"
+    ordering = ["-created_at"]
 admin.site.register(Applicant, ApplicantAdmin)
 
 class EventAdmin(admin.ModelAdmin):
